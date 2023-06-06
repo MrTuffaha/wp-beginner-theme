c721d8f7061b911095bef80b014c3820a27e7b62
Omar Tuffaha <omar_tuffaha@hotmail.com>
2023-06-06 17:23:05 +0200

    add initial css and html

--- index.php
+++ index.php
@@ -6,12 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php wp_head(); ?>
 </head>
 
 <body>
     <h1>
         hello world
     </h1>
+    <?php wp_foot(); ?>
 </body>
 
 </html>
